2cf0fbd11b750041baaeeb24e16f2aab4de08b74
Dmytro Shamatrin <dmytro.shamatrin@oracle.com>
2025-05-06 22:00:33 -0400

    Last fixes before public release

--- scripts/download_and_prepare_dataset.sh
+++ scripts/download_and_prepare_dataset.sh
@@ -3,10 +3,9 @@ DATASET_FILENAME="AmazonCat-13K_tfidf_train_ver1.svm.bz2"
 DATASET_PATH="https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/multilabel/$DATASET_FILENAME"
 PYTHON_INTERPRETER="python3"
 
-if not test -e "data/$DATASET_FILENAME"; then
+if ! test -e "data/$DATASET_FILENAME"; then
     echo "File $DATASET_FILENAME does not exist. Downloading"
     wget -P data $DATASET_PATH
-else
-    echo "File exists, preprocessing..."
-    $PYTHON_INTERPRETER scripts/prepare_data.py
 fi
+echo "File exists, preprocessing..."
+$PYTHON_INTERPRETER scripts/prepare_data.py
